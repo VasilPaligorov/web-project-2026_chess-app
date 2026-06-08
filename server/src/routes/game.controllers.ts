91797bf48e9fb3cbe10c4ead62a6a3f71ec7ae54
Martin Monkov <mbmonkov@gmail.com>
2026-06-08 13:39:26 +0300

    refactor: extract getSpectateGame into controllers folder

--- server/src/routes/game.controllers.ts
+++ server/src/routes/game.controllers.ts
@@ -29,19 +29,6 @@ export async function getCurrentGames(req: Request, res: Response) {
   res.json({ success: true, data: games });
 }
 
-export async function getSpectateGame(req: Request, res: Response) {
-  const game = await Game.findOne({ spectatorToken: req.params.token })
-    .populate('whitePlayer', PUBLIC_USER_FIELDS)
-    .populate('blackPlayer', PUBLIC_USER_FIELDS)
-    .populate('winner', PUBLIC_USER_FIELDS);
-
-  if (!game) {
-    res.status(404).json({ success: false, message: 'Game not found' });
-    return;
-  }
-  res.json({ success: true, data: game });
-}
-
 export async function getGameById(req: Request, res: Response) {
   const id = parseObjectId(req.params.id);
   if (!id) {
